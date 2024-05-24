6f4d6c3aea610554630a83ba0a56b1daa1badf78
satkar2001 <satkaracharya883@gmail.com>
2024-05-24 16:03:41 +0530

    .

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return <>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ipsam corrupti pariatur totam debitis dolorem, porro nihil similique optio nam sit odio itaque vero sapiente illum nostrum magni. Libero, repellat. 
+  <Button variant={"destructive"} >hi</Button>
+  </>
+
+}
+
+export default App
